d100e35f14b76322c018e09579ee6b9177398221
Benjamin Burgess <anathemus@gmail.com>
2018-04-04 16:46:50 -0400

    Users must now authenticate to use Financial part of site.

--- BABurgessWebApp/BABurgessWebApp/Controllers/FinancialController.cs
+++ BABurgessWebApp/BABurgessWebApp/Controllers/FinancialController.cs
@@ -7,9 +7,11 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using BABurgessWebApp.Models;
+using System.Web.Mvc.Filters;
 
 namespace BABurgessWebApp.Controllers
 {
+    [Authorize]
     public class FinancialController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
@@ -125,4 +127,5 @@ namespace BABurgessWebApp.Controllers
             base.Dispose(disposing);
         }
     }
+
 }
